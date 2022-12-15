44ca2f6259911927fbc9db5559c670ed9f90053d
WHL99 <xjv298347@gmail.com>
2022-12-15 16:37:55 +0100

    When no results returned, retrieve a random artist name from json, then write all the results in a CSV file.

--- routes/searchArtist.js
+++ routes/searchArtist.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const fetch = require('node-fetch')
 const { parse } = require('json2csv')
 const fs = require('fs');
+const randomArtistList = require('../data/random-artist-list.json')
 
 router.get('/:artist', async (req, res) => {
     const artist = req.params.artist;
@@ -23,12 +24,24 @@ router.get('/:artist', async (req, res) => {
         saveResultToCSV(artist, artistInfoCSV)
     }
     else if (artistInfo.length === 0) {
-        console.log('Sorry, no matches...')
+        const url = `https://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${getARandomArtist()}&api_key=${process.env.API_KEY}&format=json`;
+        const getArtist = await fetch(url, {
+            method: 'get',
+        })
+        const resArtist = await getArtist.json()
+        const artistInfo = await resArtist.results.artistmatches.artist.map((ele) => {
+            return {
+                name: ele.name, mbid: ele.mbid, url: ele.url, image_small: ele.image[0][`#text`],
+                image: ele.image
+            }
+        })
+        const artistInfoCSV = parse(artistInfo)
+        saveResultToCSV(artist, artistInfoCSV)
     }
 });
 
 function saveResultToCSV(artist, artistInfoCSV) {
-    fs.writeFile(`${artist}.csv`, `ufeff${artistInfoCSV}`,
+    fs.writeFile(`${artist}.csv`, artistInfoCSV,
         {
             encoding: 'utf8',
             mode: 0o666
@@ -42,5 +55,9 @@ function saveResultToCSV(artist, artistInfoCSV) {
         });
 }
 
-module.exports = router
+function getARandomArtist() {
+    const aRandomArtist = randomArtistList.artist[Math.floor(Math.random() * randomArtistList.artist.length)]
+    return aRandomArtist
+}
 
+module.exports = router
